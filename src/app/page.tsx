f6ffd3c432cd03f504e4ea2cfc21e16623631015
Mayeku Khisa <101919651+mayekukhisa@users.noreply.github.com>
2024-10-22 21:45:55 +0300

    feat: introduce feature one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
    return (
       <main className="grid h-svh place-items-center">
          <p className="p-4 text-3xl font-medium">test-project</p>
+         <p>Feature 1</p>
       </main>
    )
 }
